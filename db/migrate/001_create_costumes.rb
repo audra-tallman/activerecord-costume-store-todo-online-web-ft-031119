d9c5e1f695b9813a3ef47e18fe24883677dd0af3
Audra Tallman <audi.tallman@gmail.com>
2019-04-23 19:29:07 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,11 +2,11 @@ class CreateCostumes<ActiveRecord::Migration[5.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.integer :price
+      t.float :price
       t.string :size
       t.string :image_url
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.timestamps :created_at
+      t.timestamps :updated_at
     end
   end 
 end 
