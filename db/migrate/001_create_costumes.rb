8a70d83a79c57347e930b6f74c7122cdc83300a7
Audra Tallman <audi.tallman@gmail.com>
2019-04-23 19:17:36 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -4,7 +4,12 @@ class CreateCostumes<ActiveRecord::Migration[5.2]
       t.string :name
       t.integer :price
       t.string :size
-      
+      t.string :image_url
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
+  end 
+end 
 
 
 
